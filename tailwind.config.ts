1d2cf2967705946db002ddb73c8d8129742a6c73
CHUO Kimheng <hengk7401@gmail.com>
2025-07-26 05:48:37 +0000

    Use Khmer language Kantumruy Pro Font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['"Kantumruy Pro"', 'sans-serif'],
+        headline: ['"Kantumruy Pro"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
